c86d50171807130e0d6fb65c9ccfcd7040173f47
chapmanCC <chapboy1989@gmail.com>
2019-05-06 16:23:12 +0800

    added js t-shirt color feature

--- js/app.js
+++ js/app.js
@@ -13,3 +13,34 @@ $titleInput.change(function (e) {
         $otherTitleInput.attr("type", "hidden")
     }
 })
+
+// T-shirt color
+
+const $tshirtDesign = $("#design");
+const $tshirtColor = $("#color");
+const $colorsArray = $tshirtColor.children();
+
+$tshirtDesign.change(function (e) {
+    const tshirtcat = $(this).val();
+    const mid = $colorsArray.length/2
+    
+    $colorsArray.each( function(i, ele) { //reset all options 
+        ele.hidden = false;
+    })
+
+    if (tshirtcat === "js puns") {
+        $colorsArray.each( function(i, ele) {
+            ele.hidden = false
+            if (i >= mid) {
+                ele.hidden = true
+            }
+        })
+    } else if (tshirtcat === "heart js") {
+        $colorsArray.each( function(i, ele) {
+            ele.hidden = false
+            if (i < mid) {
+                ele.hidden = true
+            }
+        })
+    }
+})
